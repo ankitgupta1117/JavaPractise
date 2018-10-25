1139fa838ad9f5b3a3b8a83e1718abb827e3c6e8
ankitgupta1117 <ankitgupta1117@gmail.com>
2018-10-25 16:32:03 +0530

    Initial Commit

---
+++ src/com/sapient/java/innerclass/MainNestedClass.java
@@ -0,0 +1,9 @@
+package com.sapient.java.innerclass;
+
+public class MainNestedClass {
+
+    public static void main(String[] args) {
+        NestedInnerClass n = new NestedInnerClass();
+        n.fun();
+    }
+}
